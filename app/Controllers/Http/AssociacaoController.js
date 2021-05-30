804bce80d4fa57cbb5c5f3845fa1067fba8ca127
nettonucci <nettonucci@gmail.com>
2021-05-30 19:04:18 -0300

    Update AssociacaoController.js

--- app/Controllers/Http/AssociacaoController.js
+++ app/Controllers/Http/AssociacaoController.js
@@ -1,28 +1,8 @@
 const knex = require("../../../database");
+const { createInBatch, deleteInBatch } = require("./LaboratorioController");
 
 module.exports = {
-  async list(req, res) {
-    const results = await knex.select("id", "name", 'address', 'status').from("laboratorios").where({
-        status: 1
-      });
-    return res.json(results);
-  },
-
-  async show(req, res) {
-    const { id } = req.params;
-
-    const results = await knex("laboratorios")
-    .where({
-        id: id
-      }).select("id", "name", 'address', 'status')
   
-      if(results.length > 0) {
-        return res.status(201).json(results);
-      } else {
-        return res.status(404).json({ Successes: false, Message: `Laboratory not found` });
-      }
-    
-  },
 
   async create(req, res) {
     const { laboratorio_id, exame_id } = req.body;
@@ -61,32 +41,57 @@ module.exports = {
     return res.status(201).json({ Successes: true, Message: "Association successfully added" });
   },
 
-  async update(req, res) {
-    const { id } = req.params;
-    const { name, address, status } = req.body;
+  async createInBatch(req, res) {
+    const { lote } = req.body;
 
-    const results = await knex("laboratorios")
+    var errors =  []
+    var successes = []
+
+    for (var i=0; i < lote.length; i++) {
+
+    var resultsExam = await knex("exames")
     .where({
-        id: id
-      }).select("id")
+        id: lote[i].exame_id
+    }).select("status")
 
+    var resultsLab = await knex("laboratorios")
+    .where({
+      id: lote[i].laboratorio_id
+    }).select("status")
 
-      if(results.length > 0) {
-        await knex("laboratorios").update({
-            name,
-            address,
-            status
-          }).where({
-            id
-          });
-    
-        return res.status(201).json({ Successes: true, Message: "Laboratory successfully updated"});
-      } else {
-        return res.status(404).json({ Successes: false, Message: "Laboratory not found" });
-      }
+    if (resultsExam.length > 0) {
+        if (resultsExam[0].status === 0) {
+            errors.push({exam_id: lote[i].exame_id, Successes: false, Message: `Exam not active` }); 
+            continue
+        }
+    } else {
+        errors.push({exam_id: lote[i].exame_id, Successes: false, Message: `Exam not found` });
+        continue
+    }
 
-    
+    if (resultsLab.length > 0) {
+        if (resultsLab[0].status === 0) {
+            errors.push({laboratory_id: lote[i].laboratorio_id, Successes: false, Message: `Laboratory not active` }); 
+            continue
+        }
+    } else {
+        errors.push({laboratory_id: lote[i].laboratorio_id, Successes: false, Message: `Laboratory not found` });
+        continue
+    }
+
+    await knex("rel_lab_exam").insert(lote[i]);
+
+    successes.push({laboratory_id: lote[i].laboratorio_id, exam_id: lote[i].exame_id, Successes: true, Message: "Association successfully added" });
+
+    }
+    if(successes.length === 0) {
+        return res.status(400).json({errors, successes})
+    } else {
+        return res.status(201).json({successes, errors})
+    }
   },
+
+  
 
   async delete(req, res) {
     const { id } = req.params;
@@ -106,6 +111,38 @@ module.exports = {
       } else {
         return res.status(404).json({ Successes: false, Message: `Association not found` });
       }
+    
+  },
+
+  async deleteInBatch(req, res) {
+    const { lote } = req.body;
+
+    var errors =  []
+    var successes = []
+
+    for (var i=0; i < lote.length; i++) {
+        var results = await knex("rel_lab_exam")
+        .where({
+            id: lote[i].id
+          }).select("id")
+    
+        if(results.length > 0) {
+    
+            await knex("rel_lab_exam").del().where({
+                id: lote[i].id
+            });
+    
+            successes.push({assoc_id: lote[i].id, Successes: true, Message: "Association successfully deleted" })
+        } else {
+            errors.push({assoc_id: lote[i].id, Successes: false, Message: "Association not found"})
+        }
+    }
+
+    if(successes.length === 0) {
+        return res.status(400).json({errors, successes})
+    } else {
+        return res.status(201).json({successes, errors})
+    }
     
   }
 };
