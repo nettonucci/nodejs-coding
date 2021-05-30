6bf7f431c86d67d0757fe824a630af7d638b049d
nettonucci <nettonucci@gmail.com>
2021-05-30 19:20:47 -0300

    Update server.js

--- start/server.js
+++ start/server.js
@@ -7,4 +7,4 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => console.log('Server is running'));
+app.listen(process.env.PORT || 3333, () => console.log('Server is running'));
