bba6686b7c65f9e301e32d12a23925d7f8f0e327
nettonucci <nettonucci@gmail.com>
2021-05-30 15:17:42 -0300

    Update routes.js

--- start/routes.js
+++ start/routes.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const routes = express.Router();
 
+const LaboratorioController = require('../app/Controllers/Http/LaboratorioController');
+
+
+
+    routes.get('/laboratorios/listActive', LaboratorioController.list);
+    routes.get('/laboratorios/show/:id', LaboratorioController.show);
+    routes.post('/laboratorios/create', LaboratorioController.create);
+    routes.put('/laboratorios/update/:id', LaboratorioController.update);
+    routes.delete('/laboratorios/delete/:id', LaboratorioController.delete);
+
+
+
+
 module.exports = routes;
