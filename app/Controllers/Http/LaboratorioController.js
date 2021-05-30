e032de709fb034a96c38fca2aabfd2d00b550814
nettonucci <nettonucci@gmail.com>
2021-05-30 17:33:01 -0300

    Update LaboratorioController.js

--- app/Controllers/Http/LaboratorioController.js
+++ app/Controllers/Http/LaboratorioController.js
@@ -27,12 +27,28 @@ module.exports = {
   async create(req, res) {
     const { name, address } = req.body;
 
+
     await knex("laboratorios").insert({
         name,
         address
       });
 
     return res.status(201).json({ Successes: true, Message: "Laboratory successfully added" });
+  },
+
+  async createInBatch(req, res) {
+    const { lote } = req.body;
+
+    console.log()
+
+    for (var i=0; i < lote.length; i++) {
+        await knex("laboratorios").insert({
+            name: lote[i].name,
+            address: lote[i].address
+          });
+    }
+
+    return res.status(201).json({ Successes: true, Message: `${lote.length} Laboratory successfully added` });
   },
 
   async update(req, res) {
@@ -62,6 +78,42 @@ module.exports = {
     
   },
 
+  async updateInBatch(req, res) {
+    const { lote } = req.body;
+
+    var errors =  []
+    var successes = []
+
+    for (var i=0; i < lote.length; i++) {
+        var results = await knex("laboratorios")
+        .where({
+            id: lote[i].id
+          }).select("id")
+    
+    
+          if(results.length > 0) {
+            await knex("laboratorios").update(
+                lote[i]
+              ).where({
+                id: lote[i].id
+              });
+        
+            successes.push({laboratory_id: lote[i].id, Successes: true, Message: "Laboratory successfully updated" })
+          } else {
+            errors.push({laboratory_id: lote[i].id, Successes: false, Message: "Laboratory not found"})
+          }
+    }
+
+    if(successes.length === 0) {
+        return res.status(400).json({errors, successes})
+    } else {
+        return res.status(201).json({successes, errors})
+    }
+
+
+    
+  },
+
   async delete(req, res) {
     const { id } = req.params;
     
@@ -81,5 +133,36 @@ module.exports = {
       } else {
         return res.status(404).json({ Successes: false, Message: "Laboratory not found" });
       }
+  },
+
+  async deleteInBatch(req, res) {
+    const { lote } = req.body;
+
+    var errors =  []
+    var successes = []
+
+    for (var i=0; i < lote.length; i++) {
+        var results = await knex("laboratorios")
+        .where({
+            id: lote[i].id
+          }).select("id")
+    
+    
+          if(results.length > 0) {
+            await knex("laboratorios").del().where({
+                id: lote[i].id
+              });
+        
+            successes.push({laboratory_id: lote[i].id, Successes: true, Message: "Laboratory successfully deleted" })
+          } else {
+            errors.push({laboratory_id: lote[i].id, Successes: false, Message: "Laboratory not found"})
+          }
+    }
+
+    if(successes.length === 0) {
+        return res.status(400).json({errors, successes})
+    } else {
+        return res.status(201).json({successes, errors})
+    }
   }
 };
