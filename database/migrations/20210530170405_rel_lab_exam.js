145066faca05e6d803214212b5449d5e7020ebe2
nettonucci <nettonucci@gmail.com>
2021-05-30 15:17:33 -0300

    Create 20210530170405_rel_lab_exam.js

---
+++ database/migrations/20210530170405_rel_lab_exam.js
@@ -0,0 +1,20 @@
+exports.up = knex =>
+  knex.schema.createTable("rel_lab_exam", table => {
+    table.increments("id");
+    table.integer("laboratorio_id").notNullable();
+    table.integer("exame_id").notNullable();
+    table
+      .foreign("laboratorio_id")
+      .references("id")
+      .inTable("laboratorio")
+      .onDelete("SET NULL")
+      .onUpdate("CASCADE");
+    table
+      .foreign("exame_id")
+      .references("id")
+      .inTable("exames")
+      .onDelete("SET NULL")
+      .onUpdate("CASCADE");
+  });
+
+exports.down = knex => knex.schema.dropTable("rel_lab_exam");
